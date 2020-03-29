d7c80c0ba75fd16dc5b1cea41348b695be354091
kuls <42165104+hellokuls@users.noreply.github.com>
2020-03-29 08:10:12 +0800

    修改部分代码

--- src/com/kuls/filter/UserFilter.java
+++ src/com/kuls/filter/UserFilter.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebFilter(filterName = "UserFilter",urlPatterns = "")
+@WebFilter(filterName = "UserFilter",urlPatterns = "*.jsp")
 public class UserFilter implements Filter {
     public void destroy() {
     }
